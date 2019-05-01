c7bb26005696512e7252618c3a827a948858fd86
Ivica Munitic <imunitic@outlook.com>
2019-05-01 15:04:32 +0200

    Added implementation for approx_eq from float-cmp create for Tuple

--- src/tuple/mod.rs
+++ src/tuple/mod.rs
@@ -1,62 +1,94 @@
+use float_cmp::ApproxEq;
+use std::f64;
 
-struct Tuple {
+pub struct Tuple {
     x: f64,
     y: f64,
     z: f64,
     w: f64
 }
 
-fn tuple(x: f64, y: f64, z: f64, w: f64) -> Tuple {
-    return Tuple { x: x, y: y, z: z, w: w};
+impl Tuple {
+    fn tuple(x: f64, y: f64, z: f64, w: f64) -> Tuple {
+        return Tuple { x, y, z, w};
+    }
+
+    fn point(x: f64, y: f64, z: f64) -> Tuple {
+        return Tuple::tuple(x, y, z, 1.0);
+    }
+
+    fn vector(x: f64, y: f64, z: f64) -> Tuple {
+        return Tuple::tuple(x, y, z, 0.0);
+    }
+
+    fn eq(&self, other: &Tuple) -> bool {
+        self.approx_eq(other, f64::EPSILON, 0)
+    }
 }
 
-fn point(x: f64, y: f64, z: f64) -> Tuple {
-    return tuple(x, y, z, 1.0);
+impl ApproxEq for Tuple {
+    type Flt = f64;
+
+    fn approx_eq(&self, other: &Tuple, epsilon: f64, ulps: i64) -> bool {
+        (self.x == other.x &&
+        self.y == other.y &&
+        self.z == other.z &&
+        self.w == other.w) ||
+        (self.x.approx_eq(&other.x, epsilon, ulps) &&
+        self.y.approx_eq(&other.y, epsilon, ulps) &&
+        self.z.approx_eq(&other.z, epsilon, ulps) &&
+        self.w.approx_eq(&other.w, epsilon, ulps))
+    }
 }
 
-fn vector(x: f64, y: f64, z: f64) -> Tuple {
-    return tuple(x, y, z, 0.0);
-}
 
 #[cfg(test)]
 mod tests {
-    use crate::tuple::tuple;
-    use crate::tuple::vector;
-    use crate::tuple::point;    
+    use crate::tuple::Tuple;
+    use float_cmp::ApproxEq;
+    use std::f64;
+
 
     #[test]
+    fn test_approx_eq() {
+        let t1 = Tuple::tuple(4.3, -4.2, 3.1, 1.0);
+        let t2 = Tuple::tuple(4.3, -4.2, 3.1, 1.0);
+
+        assert!(t1.eq(&t2));
+    }
+    #[test]
     fn test_tuple_as_point() {
-        let t = tuple(4.3, -4.2, 3.1, 1.0);
-        assert_eq!(t.x, 4.3);
-        assert_eq!(t.y, -4.2);
-        assert_eq!(t.z, 3.1);
-        assert_eq!(t.w, 1.0);
+        let t = Tuple::tuple(4.3, -4.2, 3.1, 1.0);
+        assert!(t.x.approx_eq(&4.3, f64::EPSILON, 0));
+        assert!(t.y.approx_eq(&-4.2, f64::EPSILON, 0));
+        assert!(t.z.approx_eq(&3.1, f64::EPSILON, 0));
+        assert!(t.w.approx_eq(&1.0, f64::EPSILON, 0));
     }
 
     #[test]
     fn test_tuple_as_vector() {
-        let t = tuple(4.3, -4.2, 3.1, 0.0);
-        assert_eq!(t.x, 4.3);
-        assert_eq!(t.y, -4.2);
-        assert_eq!(t.z, 3.1);
-        assert_eq!(t.w, 0.0);
+        let t = Tuple::tuple(4.3, -4.2, 3.1, 0.0);
+        assert!(t.x.approx_eq(&4.3, f64::EPSILON, 0));
+        assert!(t.y.approx_eq(&-4.2, f64::EPSILON, 0));
+        assert!(t.z.approx_eq(&3.1, f64::EPSILON, 0));
+        assert!(t.w.approx_eq(&0.0, f64::EPSILON, 0));
     }
 
     #[test]
     fn test_point_fn_creates_a_point() {
-        let t = point(4.3, -4.2, 3.1);
-        assert_eq!(t.x, 4.3);
-        assert_eq!(t.y, -4.2);
-        assert_eq!(t.z, 3.1);
-        assert_eq!(t.w, 1.0);
+        let t = Tuple::point(4.3, -4.2, 3.1);
+        assert!(t.x.approx_eq(&4.3, f64::EPSILON, 0));
+        assert!(t.y.approx_eq(&-4.2, f64::EPSILON, 0));
+        assert!(t.z.approx_eq(&3.1, f64::EPSILON, 0));
+        assert!(t.w.approx_eq(&1.0, f64::EPSILON, 0));
     }
 
     #[test]
     fn test_vector_fn_creates_a_vector() {
-        let t = vector(4.3, -4.2, 3.1);
-        assert_eq!(t.x, 4.3);
-        assert_eq!(t.y, -4.2);
-        assert_eq!(t.z, 3.1);
-        assert_eq!(t.w, 0.0);
+        let t = Tuple::vector(4.3, -4.2, 3.1);
+        assert!(t.x.approx_eq(&4.3, f64::EPSILON, 0));
+        assert!(t.y.approx_eq(&-4.2, f64::EPSILON, 0));
+        assert!(t.z.approx_eq(&3.1, f64::EPSILON, 0));
+        assert!(t.w.approx_eq(&0.0, f64::EPSILON, 0));
     }
 }

0123d95142c02d513be756c45ce6b421fd11332d
Ivica Munitic <imunitic@outlook.com>
2019-05-01 13:58:01 +0200

    Add tuple function that given x,y,z, and w creates a tuple

--- src/tuple/mod.rs
+++ src/tuple/mod.rs
@@ -6,23 +6,27 @@ struct Tuple {
     w: f64
 }
 
+fn tuple(x: f64, y: f64, z: f64, w: f64) -> Tuple {
+    return Tuple { x: x, y: y, z: z, w: w};
+}
+
 fn point(x: f64, y: f64, z: f64) -> Tuple {
-    return Tuple { x: x, y: y, z: z, w: 1.0};
+    return tuple(x, y, z, 1.0);
 }
 
 fn vector(x: f64, y: f64, z: f64) -> Tuple {
-    return Tuple {x: x, y: y, z: z, w: 0.0};
+    return tuple(x, y, z, 0.0);
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::tuple::tuple;
     use crate::tuple::vector;
-    use crate::tuple::point;
-    use crate::tuple::Tuple;
+    use crate::tuple::point;    
 
     #[test]
     fn test_tuple_as_point() {
-        let t = Tuple { x: 4.3, y: -4.2, z: 3.1, w: 1.0};
+        let t = tuple(4.3, -4.2, 3.1, 1.0);
         assert_eq!(t.x, 4.3);
         assert_eq!(t.y, -4.2);
         assert_eq!(t.z, 3.1);
@@ -31,7 +35,7 @@ mod tests {
 
     #[test]
     fn test_tuple_as_vector() {
-        let t = Tuple { x: 4.3, y: -4.2, z: 3.1, w: 0.0};
+        let t = tuple(4.3, -4.2, 3.1, 0.0);
         assert_eq!(t.x, 4.3);
         assert_eq!(t.y, -4.2);
         assert_eq!(t.z, 3.1);

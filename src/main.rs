8e0a52d888eb411eec03e2596949e0183a59d2e2
Ivica Munitic <imunitic@outlook.com>
2019-05-07 21:31:06 +0200

    Add examples of usage for Tuple module with a simple moving of a projectile in an environment

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,44 @@
 mod tuple;
+use tuple::Tuple;
+
+#[derive(Copy, Clone)]
+struct Projectile {
+	position: Tuple,
+	velocity: Tuple
+}
+
+#[derive(Copy, Clone)]
+struct Environment {
+	gravity: Tuple,
+	wind: Tuple
+}
+
+fn tick(env: Environment, proj: Projectile) -> Projectile {
+	let position = proj.position + proj.velocity;
+	let velocity = proj.velocity + env.gravity + env.wind;
+
+	Projectile { position, velocity}
+}
+
+
 fn main() {
-    println!("Hello, world!");
+	let mut p = Projectile { position: Tuple::point(0.0, 1.0, 0.0), 
+		velocity: Tuple::vector(1.0, 1.0, 0.0).normalize()};
+	let e = Environment {
+		gravity: Tuple::vector(0.0, -0.1, 0.0),
+		wind: Tuple::vector(-0.01, 0.0, 0.0)
+	};
+
+	let mut pos = p.position.y;
+	let mut counter = 1;
+
+	while pos > 0.0 {
+		p = tick(e, p);
+		
+		println!("{:?}", pos);
+		
+		pos = p.position.y;
+		counter = counter +  1;
+	}
+	println!("Run tick {:?} times", counter);
 }
--- src/tuple/mod.rs
+++ src/tuple/mod.rs
@@ -2,10 +2,11 @@ use float_cmp::ApproxEq;
 use std::f64;
 use std::ops::{Add, Sub, Neg, Mul, Div};
 
+#[derive(Copy, Clone)]
 pub struct Tuple {
-    x: f64,
-    y: f64,
-    z: f64,
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
     w: f64
 }
 
@@ -16,40 +17,40 @@ impl Tuple {
         Tuple { x, y, z, w}
     }
 
-    fn point(x: f64, y: f64, z: f64) -> Tuple {
+    pub fn point(x: f64, y: f64, z: f64) -> Tuple {
         Tuple::tuple(x, y, z, 1.0)
     }
 
-    fn vector(x: f64, y: f64, z: f64) -> Tuple {
+    pub fn vector(x: f64, y: f64, z: f64) -> Tuple {
         Tuple::tuple(x, y, z, 0.0)
     }
 
-    fn eq(&self, other: &Tuple) -> bool {
-        self.approx_eq(other, Tuple::EPSILON, 0)
+    pub fn eq(self, other: Tuple) -> bool {
+        self.approx_eq(&other, Tuple::EPSILON, 0)
     }
 
-    fn magnitude(&self) -> f64 {
+    pub fn magnitude(self) -> f64 {
         (self.x.powf(2.0) +
          self.y.powf(2.0) +
          self.z.powf(2.0) +
          self.w.powf(2.0)).sqrt()
     }
 
-    fn normalize(&self) -> Tuple {
+    pub fn normalize(self) -> Tuple {
         Tuple::tuple(self.x / self.magnitude(),
                      self.y / self.magnitude(),
                      self.z / self.magnitude(),
                      self.w / self.magnitude())
     }
 
-    fn dot(&self, other: &Tuple) -> f64 {
+    pub fn dot(self, other: Tuple) -> f64 {
         self.x * other.x +
         self.y * other.y +
         self.z * other.z +
         self.w * other.w
     }
 
-    fn cross(&self, other: &Tuple) -> Tuple {
+    pub fn cross(self, other: Tuple) -> Tuple {
         Tuple::vector(self.y * other.z - self.z * other.y,
                       self.z * other.x - self.x * other.z,
                       self.x * other.y - self.y * other.x)
@@ -136,7 +137,7 @@ mod tests {
         let t1 = Tuple::tuple(4.3, -4.2, 3.1, 1.0);
         let t2 = Tuple::tuple(4.3, -4.2, 3.1, 1.0);
 
-        assert!(t1.eq(&t2));
+        assert!(t1.eq(t2));
     }
     #[test]
     fn test_tuple_as_point() {
@@ -180,7 +181,7 @@ mod tests {
         let t2 = Tuple::tuple(-2.0, 3.0, 1.0, 0.0);
         let result = t1 + t2;
         let expected = Tuple::tuple(1.0, 1.0, 6.0, 1.0);
-        assert!(result.eq(&expected));
+        assert!(result.eq(expected));
     }
 
     #[test]
@@ -190,7 +191,7 @@ mod tests {
         let result = t1 - t2;
         let expected = Tuple::vector(-2.0, -4.0, -6.0);
 
-        assert!(result.eq(&expected));
+        assert!(result.eq(expected));
     }
 
     #[test]
@@ -200,7 +201,7 @@ mod tests {
         let result = t1 - t2;
         let expected = Tuple::point(-2.0, -4.0, -6.0);
 
-        assert!(result.eq(&expected));
+        assert!(result.eq(expected));
     }
 
     #[test]
@@ -210,7 +211,7 @@ mod tests {
         let result = t1 - t2;
         let expected = Tuple::vector(-2.0, -4.0, -6.0);
 
-        assert!(result.eq(&expected));
+        assert!(result.eq(expected));
     }
 
     #[test]
@@ -219,7 +220,7 @@ mod tests {
         let result = -t;
         let expected = Tuple::tuple(-1.0, 2.0, -3.0, 4.0);
 
-        assert!(result.eq(&expected));
+        assert!(result.eq(expected));
     }
 
     #[test]
@@ -228,7 +229,7 @@ mod tests {
         let result = t * 3.5;
         let expected = Tuple::tuple(3.5, -7.0, 10.5, -14.0);
 
-        assert!(result.eq(&expected));
+        assert!(result.eq(expected));
     }
 
     #[test]
@@ -237,7 +238,7 @@ mod tests {
         let result = t * 0.5;
         let expected = Tuple::tuple(0.5, -1.0, 1.5, -2.0);
 
-        assert!(result.eq(&expected));
+        assert!(result.eq(expected));
     }
 
     #[test]
@@ -246,7 +247,7 @@ mod tests {
         let result = t / 2.0;
         let expected = Tuple::tuple(0.5, -1.0, 1.5, -2.0);
 
-        assert!(result.eq(&expected));
+        assert!(result.eq(expected));
     }
 
     #[test]
@@ -275,20 +276,20 @@ mod tests {
         let result = t1.normalize();
         let expected = Tuple::vector(1.0, 0.0, 0.0);
 
-        assert!(result.eq(&expected));
+        assert!(result.eq(expected));
 
         let t2 = Tuple::vector(1.0, 2.0, 3.0);
         let result2 = t2.normalize();
         let expected2 = Tuple::vector(0.26726, 0.53452, 0.80178);
 
-        assert!(result2.eq(&expected2))
+        assert!(result2.eq(expected2))
     }
 
     #[test]
     fn test_dot_product() {
         let t1 = Tuple::vector(1.0, 2.0, 3.0);
         let t2 = Tuple::vector(2.0, 3.0, 4.0);
-        let result = t1.dot(&t2);
+        let result = t1.dot(t2);
         let expected = 20.0;
 
         assert!(result.approx_eq(&expected, Tuple::EPSILON, 0));
@@ -299,14 +300,14 @@ mod tests {
         let t1 = Tuple::vector(1.0, 2.0, 3.0);
         let t2 = Tuple::vector(2.0, 3.0, 4.0);
 
-        let result1 = t1.cross(&t2);
+        let result1 = t1.cross(t2);
         let expected1 = Tuple::vector(-1.0, 2.0, -1.0);
 
-        assert!(result1.eq(&expected1));
+        assert!(result1.eq(expected1));
 
-        let result2 = t2.cross(&t1);
+        let result2 = t2.cross(t1);
         let expected2 = Tuple::vector(1.0, -2.0, 1.0);
 
-        assert!(result2.eq(&expected2));
+        assert!(result2.eq(expected2));
     }
 }

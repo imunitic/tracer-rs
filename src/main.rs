14acf92ce25d64e587779670558af9bf78b3dbf2
Ivica Munitic <imunitic@outlook.com>
2019-05-28 22:11:59 +0200

    Added .DS_Store to ignore pattern

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-mod tuple;
-mod color;
 mod canvas;
+mod color;
+mod tuple;
 
 use canvas::Canvas;
 use color::Color;
@@ -9,55 +9,56 @@ use tuple::Tuple;
 
 #[derive(Copy, Clone)]
 struct Projectile {
-	position: Tuple,
-	velocity: Tuple
+    position: Tuple,
+    velocity: Tuple,
 }
 
 #[derive(Copy, Clone)]
 struct Environment {
-	gravity: Tuple,
-	wind: Tuple
+    gravity: Tuple,
+    wind: Tuple,
 }
 
 fn tick(env: Environment, proj: Projectile) -> Projectile {
-	let position = proj.position + proj.velocity;
-	let velocity = proj.velocity + env.gravity + env.wind;
+    let position = proj.position + proj.velocity;
+    let velocity = proj.velocity + env.gravity + env.wind;
 
-	Projectile { position, velocity}
+    Projectile { position, velocity }
 }
 
-
 fn main() {
-	let mut p = Projectile { position: Tuple::point(0.0, 1.0, 0.0), 
-		velocity: Tuple::vector(1.0, 1.8, 0.0).normalize() * 11.25};
-	let e = Environment {
-		gravity: Tuple::vector(0.0, -0.1, 0.0),
-		wind: Tuple::vector(-0.01, 0.0, 0.0)
-	};
+    let mut p = Projectile {
+        position: Tuple::point(0.0, 1.0, 0.0),
+        velocity: Tuple::vector(1.0, 1.8, 0.0).normalize() * 11.25,
+    };
+    let e = Environment {
+        gravity: Tuple::vector(0.0, -0.1, 0.0),
+        wind: Tuple::vector(-0.01, 0.0, 0.0),
+    };
 
-	let color = Color::new(0.5, 0.88, 1.0);
+    let color = Color::new(0.5, 0.88, 1.0);
 
-	let mut canvas = Canvas::new(900, 550);
+    let mut canvas = Canvas::new(900, 550);
 
-	let mut pos = p.position.y;
+    let mut pos = p.position.y;
 
-	while pos > 0.0 {
-		p = tick(e, p);
-		let x = p.position.x as i64;
-		let y = canvas.height as i64 - p.position.y as i64;
+    while pos > 0.0 {
+        p = tick(e, p);
+        let x = p.position.x as i64;
+        let y = canvas.height as i64 - p.position.y as i64;
 
-		if x < 0 || y < 0 {
-			pos = p.position.y;
-			continue;
-		}
+        if x < 0 || y < 0 {
+            pos = p.position.y;
+            continue;
+        }
 
-		if x > 900 || y > 550 {
-			pos = p.position.y;
-			continue;
-		}
-		canvas.write_pixel(x as usize, y as usize, color);
-		
-		pos = p.position.y;
-	}
-	println!("{}", canvas.to_ppm());
+        if x > 900 || y > 550 {
+            pos = p.position.y;
+            continue;
+        }
+        canvas.write_pixel(x as usize, y as usize, color);
+
+        pos = p.position.y;
+    }
+    println!("{}", canvas.to_ppm());
 }
